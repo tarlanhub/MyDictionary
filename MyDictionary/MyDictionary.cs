381b185389756582e496069ce1de9c954be9262d
pdesgn <pdesgn@DESKTOP-LC4MO2N>
2021-02-26 01:31:38 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        List<TKey> keys;
+        List<TValue> values;
+        public MyDictionary()
+        {
+            keys = new List<TKey>();
+            values = new List<TValue>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            keys.Add(key);
+            values.Add(value);
+        }
+        public void Control(TKey akey)
+        {
+            Console.WriteLine(keys.IndexOf(akey) + "Key's Value = " + values[keys.IndexOf(akey)]);
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> customers = new MyDictionary<int, string>();
+
+            customers.Add(1, "Amy");
+            customers.Add(2, "2.Amy");
+            customers.Add(3, "3.Amy");
+
+            customers.Control(1);
+            customers.Control(2);
+            customers.Control(3);
+        }
+    }
+}
